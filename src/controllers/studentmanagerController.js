f9cb9e1856d8bcff5f8d0a6a8a9501d365438ddf
Jie <1850549503@qq.com>
2018-08-29 15:21:19 +0800

    完成学生列表搜索

--- src/controllers/studentmanagerController.js
+++ src/controllers/studentmanagerController.js
@@ -1,7 +1,38 @@
+const xtpl = require('xtpl');
+const path = require('path')
+const MongoClient = require('mongodb').MongoClient
+// Connection URL
+const url = 'mongodb://localhost:27017';
 
 
+// Database Name
+const dbName = 'jie';
 //最终处理 , 返回获取到的学生列表页面
 exports.getstudentmanagerPage = (req,res)=>{
-    res.send('这是学生列表页面 , 待完善....')
+
+    const keyword = req.query.keyword || ""
+
+    // Use connect method to connect to the server
+    MongoClient.connect(url,{ useNewUrlParser: true }, function(err, client) {
+    
+        // 拿到db对象
+        const db = client.db(dbName);
+
+        //拿到集合
+        const collection = db.collection('studentInfo');
+
+        collection.find({name:{$regex:keyword}}).toArray((req,docs)=>{
+            // 关闭与数据库的连接 
+            client.close();
+
+            xtpl.renderFile(path.join(__dirname,'../statics/views/list.html'),{
+                students:docs,
+                keyword
+            },function(error,content){
+                res.send(content)
+            });
+        })
+    });
+
 }
 
